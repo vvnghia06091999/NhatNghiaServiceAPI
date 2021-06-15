f229b3a5738fa1e0d6c6ead0499cb04d12a8107e
Vo Van Nghia <vvnghia06091999@gmail.com>
2021-06-15 20:58:56 +0700

    deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ paypal.configure({
 });
 
 var corsOptions = {
-    origin: "http://localhost:8080"
+    origin: "http://ec2-13-212-49-158.ap-southeast-1.compute.amazonaws.com:8080"
 };
 
 var transporter = nodemailer.createTransport({
@@ -47,8 +47,8 @@ app.post('/pay',function(req,res){
             "payment_method": "paypal"
         },
         "redirect_urls": {
-            "return_url": "http://localhost:8080/controller/thanhToanPayPal",
-            "cancel_url": "http://localhost:8080/controller/errors"
+            "return_url": "http://ec2-13-212-49-158.ap-southeast-1.compute.amazonaws.com:8080/NhatNghiaWeb/thanhToanPayPal",
+            "cancel_url": "http://ec2-13-212-49-158.ap-southeast-1.compute.amazonaws.com:8080/NhatNghiaWeb/errors"
         },
         "transactions": [{
             "item_list": {
